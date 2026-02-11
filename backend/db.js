49f581dbac880dd6e2e8492d6f0035def9cee74f
Gogula Danarao <your_github_email@gmail.com>
2026-02-11 14:31:03 +0530

    Updated db.js to use environment variables

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,18 @@
-//db.js//
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Dana@630",
-  database: "testdb"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
-  if (err) console.log(err);
-  else console.log("MySQL Connected");
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("MySQL Connected");
+  }
 });
 
 module.exports = db;
